11e4b7177149b4ccca289f150d71374d66451593
Monty Rasmussen <montyr75@gmail.com>
2017-09-19 14:28:22 -0600

    MD component fixes

--- lib/app_header.dart
+++ lib/app_header.dart
@@ -1,11 +1,12 @@
 import 'package:angular/angular.dart';
+import 'package:angular_components/angular_components.dart';
 
 import 'src/services/database_service.dart';
 
 @Component(selector: 'app-header',
     templateUrl: 'app_header.html',
     styleUrls: const ['app_header.css'],
-    directives: const [CORE_DIRECTIVES]
+    directives: const [CORE_DIRECTIVES, materialDirectives]
 )
 class AppHeader {
   final DatabaseService dbService;
